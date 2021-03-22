7e2ad493c925eb91f6f58f405346ebbbaf8b92a1
Hanxun Jin <hanxun_jin@brown.edu>
2021-03-22 00:34:21 -0400

    Add files via upload

---
+++ graph1_barplot.js
@@ -0,0 +1,160 @@
+// Set up SVG object with width, height and margin
+let svg_barplot = d3.select("#graph1")
+    .append("svg")
+    .attr("width", graph_1_width)
+    .attr("height", graph_1_height)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Create a linear scale for the x axis (Total sales)
+let x_bar = d3.scaleLinear()
+    .range([40, graph_1_width - margin.left - margin.right-60]);
+
+// Create a scale band for the y axis (Name of the video games)
+let y_bar = d3.scaleBand()
+    .range([0, graph_1_height - margin.top - margin.bottom])
+    .padding(0.1); 
+
+
+let countRef = svg_barplot.append("g");
+
+// Set up reference to y axis label to update text in setData
+
+let y_axis_label = svg_barplot.append("g");
+
+// Add x-axis label
+svg_barplot.append("text")
+    .attr("transform", `translate(${(graph_1_width - margin.left - margin.right) / 2}, ${(graph_1_height - margin.top - margin.bottom) + 20})`)
+    .style("text-anchor", "middle")
+    .style("font-size", 12)
+    .text("Total sales (in millions)");
+
+// Add y-axis label
+let y_axis_text = svg_barplot.append("text")
+    .attr("transform", `translate(-120, ${(graph_1_height - margin.top - margin.bottom)/2})`)
+    .style("text-anchor", "middle")
+    .style("font-size", 12)
+    .text("Games");
+
+// Add chart title
+let title = svg_barplot.append("text")
+    .attr("transform", `translate(${(graph_1_width - margin.left - margin.right) / 2}, -20)`)
+    .style("text-anchor", "middle")
+    .style("font-size", 15)
+
+// Define color scale
+let color = d3.scaleOrdinal()
+    .range(d3.quantize(d3.interpolateHcl("#66a0e2", "#81c2c3"), NUM_EXAMPLES));
+
+  
+ function TotalData() {
+
+    let process_data = cleanData(data, function(a, b) {return parseFloat(b.Global_Sales)-parseFloat(a.Global_Sales)}, NUM_EXAMPLES);
+    
+    x_bar.domain([0, d3.max(process_data, function(d) { return parseFloat(d.Global_Sales); })]);
+    // Update the y axis domains with the desired attribute
+    y_bar.domain(process_data.map(function(d) { return d.Name }));
+    color.domain(process_data.map(function(d) { return d.Name }));
+    // Render y-axis label
+    y_axis_label.call(d3.axisLeft(y_bar).tickSize(0).tickPadding(2)).attr("transform", `translate(40, 0)`);
+
+    let bars = svg_barplot.selectAll("rect").data(process_data);
+
+    // Render the bar elements on the DOM
+    bars.enter()
+        .append("rect")
+        .merge(bars)
+        .transition()
+        .duration(1000)
+        .attr("fill", function(d) { return color(d.Name) })
+        .attr("x", x_bar(0))
+        .attr("y", function(d) { return y_bar(d.Name); })
+        .attr("width", function(d) { return x_bar(d.Global_Sales); })
+        .attr("height",  y_bar.bandwidth())
+
+
+    let counts = countRef.selectAll("text").data(process_data);
+        // Render the text elements on the DOM
+        counts.enter()
+            .append("text")
+            .merge(counts)
+            .attr("x", function(d) { return x_bar(d.Global_Sales) + 45; })
+            .attr("y", function(d) { return y_bar(d.Name) + 12; })
+            .style("text-anchor", "start")
+            .text(function(d) {return d.Global_Sales;});
+        
+        min_year = d3.min(data, function(d) { return d.Year; })
+        max_year = d3.max(data, function(d) { if (d.Year!="N/A") return d.Year;})
+        title.text(`Top 10 best seller video games in from ${min_year} to ${max_year}`)
+        // Remove elements not in use if fewer groups in new dataset
+        bars.exit().remove();
+        counts.exit().remove();
+ }    
+
+
+
+    
+ function updateData(year) {
+
+    // Filter data by year
+    let filteredData = data.filter(function(a) {
+        return year==a.Year;
+
+    });
+
+    process_data = cleanData(filteredData, function(a, b) {return parseFloat(b.Global_Sales)-parseFloat(a.Global_Sales)}, NUM_EXAMPLES);
+
+    //console.log( process_data)
+    // Update the x axis domain with the max sales of the provided data
+    x_bar.domain([0, d3.max(process_data, function(d) { return parseFloat(d.Global_Sales); })]);
+
+    //console.log( process_data)
+    //console.log( d3.max(process_data, function(d) { return parseFloat(d.Global_Sales);}))
+    // Update the y axis domains with the desired attribute
+    y_bar.domain(process_data.map(function(d) { return d.Name }));
+    color.domain(process_data.map(function(d) { return d.Name }));
+
+    // Render y-axis label
+    y_axis_label.call(d3.axisLeft(y_bar).tickSize(0).tickPadding(10)).attr("transform", `translate(40, 0)`);
+    let bars = svg_barplot.selectAll("rect").data(process_data);
+
+    // Render the bar elements on the DOM
+    bars.enter()
+        .append("rect")
+        .merge(bars)
+        .transition()
+        .duration(1000)
+        .attr("fill", function(d) { return color(d.Name) })
+        .attr("x", x_bar(0))
+        .attr("y", function(d) { return y_bar(d.Name); })
+        .attr("width", function(d) { return x_bar(d.Global_Sales); })
+        .attr("height",  y_bar.bandwidth())
+
+/*
+        In lieu of x-axis labels, display the count of the artist next to its bar on the
+        bar plot.
+     */
+    let counts = countRef.selectAll("text").data(process_data);
+        // Render the text elements on the DOM
+        counts.enter()
+            .append("text")
+            .merge(counts)
+            .transition()
+            .duration(1000)
+            .attr("x", function(d) { return x_bar(d.Global_Sales) + 45; })
+            .attr("y", function(d) { return y_bar(d.Name) + 12; })
+            .style("text-anchor", "start")
+            .text(function(d) {return d.Global_Sales;});
+        
+        title.text(`Top 10 best seller video games in ${year}`)
+        // Remove elements not in use if fewer groups in new dataset
+        bars.exit().remove();
+        counts.exit().remove();
+
+
+}
+
+function cleanData(data, comparator, numExamples) {
+    data.sort(comparator);
+    return data.slice(0,numExamples)
+    }
---
+++ graph2_pie.js
@@ -0,0 +1,151 @@
+let svg_pie = d3.select("#graph2")
+    .append("svg")
+    .attr("width", graph_3_width)
+    .attr("height", graph_3_height)
+    .append("g")
+    .attr("transform", `translate(${graph_2_width/2-130}, ${graph_2_height/2+150})`);
+
+let radius = 200;
+
+let pro_countdata;
+let arc; 
+let piedata; 
+
+let label = svg_pie.append("g");
+
+
+ 
+
+function graph2_update(cur_genre) {
+//Create data set for marker
+// Filter data by genre
+//d3.selectAll("text").remove();
+svg_pie.selectAll("text").remove();
+
+let genre_data;
+genre_data = data.filter(function(a) {
+  return cur_genre==a.Genre;
+
+  });
+
+ //console.log(genre_data)
+//calculate how many publisher for each
+let countdata = d3.nest()
+  .key(function(d) { return d.Publisher; })
+  .rollup(function(w) { return w.length; })
+  .entries(genre_data)
+  .sort(function(a, b) {return parseInt(b.value)-parseInt(a.value)})
+
+let cutoff = 5;
+//store the data for percentage cal
+pro_countdata  = countdata.slice(0,cutoff);
+
+/*
+let other_countdata  = countdata.slice(cutoff,countdata.length)
+
+let other_count = 0;
+for(i=0;i<other_countdata.length;i++){
+  other_count = other_count+other_countdata[i].value; 
+}
+*/
+//pro_countdata.push({key: "Others", value:other_count });
+//console.log(pro_countdata)
+
+
+
+// set the color scale
+let piecolor = d3.scaleOrdinal(d3.schemeCategory10);
+
+
+var pie = d3.pie().value(function(d) {return d.value; }).sort(null);
+
+arc = d3.arc().outerRadius(radius).innerRadius(0);
+
+piedata = pie(pro_countdata);
+
+//console.log(path)
+let u = svg_pie.selectAll("path").data(piedata);
+
+    u.enter()
+    .append("path")
+    .merge(u)
+    .transition()
+    .duration(1000)
+    .attr("d", arc)
+    .attr('fill', function(d){ return(piecolor(d.data.key)) })
+    .attr("stroke", "white")
+    .style("stroke-width", "2px")
+    .style("opacity", 0.5)   
+// Chart title
+//title_map.text(`The most popular genre per region`);
+
+// Now add the annotation. Use the centroid method to get the best coordinates
+let label_out = label.selectAll("text").data(piedata);
+
+label_out.enter()
+  .append('text')
+  .merge(label_out)
+  .transition()
+  .duration(1000)
+  .text(function(d){ return d.data.key})
+  .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")";  })
+  .style("text-anchor", "middle")
+  .style("font-size", 12)
+
+u.exit().remove();
+label_out.exit().remove();
+
+// Add chart title
+let pietitle = svg_pie.append("text")
+    .attr("transform", `translate(${(graph_3_width - margin.left - margin.right) / 2-350}, ${-270})`)
+    .style("text-anchor", "middle")
+    .style("font-size", 15);
+pietitle.text(`Top 5 publisher in the Genre: ${cur_genre}`);  
+
+
+}
+
+
+function calPercent(){
+  //console.log(pro_countdata)
+  
+  
+  //svg_pie.selectAll("text").remove();
+  
+  // total value:
+  let total_count = 0;
+  for(i=0;i<pro_countdata.length;i++){
+    total_count = total_count+pro_countdata[i].value; 
+  }
+  
+  // cal percentage:
+  for(i=0;i<pro_countdata.length;i++){
+    pro_countdata[i].value =((pro_countdata[i].value/ total_count)*100).toFixed(2);
+  
+  }
+  
+  
+  let percent_text = svg_pie.selectAll("thistext").data(piedata);
+  
+  //console.log(pro_countdata)
+  
+  percent_text.enter()
+    .append("text")
+    .merge(percent_text)
+    .transition()
+    .duration(1000)
+    .text(function(d){ return d.data.value+"%"})
+    //.attr("transform", function(d) { return "translate(" + arc.centroid(d)+10 + ")";  })
+    .attr("transform", function(d) {let cen = arc.centroid(d),
+        x = cen[0],y = cen[1],h = Math.sqrt(x*x + y*y);
+      return "translate(" + (x/h * (radius+20)) + ',' +(y/h * (radius+20)) +  ")"; 
+    })
+    .style("text-anchor", "middle")
+    .style("font-size", 15)
+  
+  
+    percent_text.exit().remove();
+  
+  
+  }
+  
